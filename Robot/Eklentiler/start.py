435efa491202cdc6fcf11685b25db7fcd7440320
Muhammet Akbaş <muhammetkbs.dev@gmail.com>
2024-03-04 18:54:43 +0300

    delete unused variables

--- Robot/Eklentiler/start.py
+++ Robot/Eklentiler/start.py
@@ -14,10 +14,6 @@ async def start_buton(client: Client, message: Message):
                                     quote=True,
                                     disable_web_page_preview=True
                                     )
-    uye_id = message.from_user.id
-    uye_nick = f"@{message.from_user.username}" if message.from_user.username else f"{message.from_user.first_name or ''} {message.from_user.last_name or ''}".strip()
-    uye_adi = f"{message.from_user.first_name or ''} {message.from_user.last_name or ''}".strip()
-    referans_iæd = message.command[1] if len(message.command) > 1 else None
     # ------------------------------------------------------------- Başlangıç >
     mesaj = f"""Merhaba **{message.from_user.first_name}**, ----------- Hoşgeldin!
 
